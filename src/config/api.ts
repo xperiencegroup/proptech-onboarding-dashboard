671bc4db6fc4e969c0d1874e0997d2190516aeb6
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-05-27 16:14:24 -0600

    Rutas utilizadas para manipular tabla de leads

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,8 +2,8 @@ export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export const API_ROUTES = {
   leads: {
-    // create: `${API_URL}/api/v1/leads`,
-    // update: (id: string) => `${API_URL}/api/v1/leads/${id}`,
+    create: `${API_URL}/api/v1/lead`,
+    update: (id: string) => `${API_URL}/api/v1/lead/${id}`,
     email: `${API_URL}/api/v1/onboardingEmail`,
   },
 } as const;
